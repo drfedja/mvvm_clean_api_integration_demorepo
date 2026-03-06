ce35a3f54da0fb8878d64f85a9a439a7899dd0e1
Fedja Drndarski <fdrndarski@gmail.com>
2026-03-06 14:12:39 +0100

    feat: parallelize repo details API calls with async/await
    - fetch repo header and tags concurrently

--- features/testapp-feat/data/src/main/java/com/nfinnova/data/usecases/GetRepoDetailsUseCaseImpl.kt
+++ features/testapp-feat/data/src/main/java/com/nfinnova/data/usecases/GetRepoDetailsUseCaseImpl.kt
@@ -3,32 +3,45 @@ package com.nfinnova.data.usecases
 import com.nfinnova.domain.models.RepoDetailsData
 import com.nfinnova.domain.repository.GitHubRepository
 import com.nfinnova.domain.usecases.GetRepoDetailsUseCase
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 internal class GetRepoDetailsUseCaseImpl @Inject constructor(
     private val repository: GitHubRepository
 ) : GetRepoDetailsUseCase {
 
-    override suspend fun invoke(user: String, repo: String): Result<RepoDetailsData> {
-        return try {
-            val headerResult = repository.getRepoHeaderData(user, repo)
-            val tagsResult = repository.getRepoTags(user, repo)
+    override suspend fun invoke(user: String, repo: String): Result<RepoDetailsData> =
+        coroutineScope {
+            try {
+                val headerDeferred = async {
+                    repository.getRepoHeaderData(user, repo)
+                }
 
-            if (headerResult.isSuccess) {
-                val header = headerResult.getOrThrow()
-                val tags = tagsResult.getOrElse { emptyList() }
+                val tagsDeferred = async {
+                    repository.getRepoTags(user, repo)
+                }
 
-                Result.success(
-                    RepoDetailsData(
-                        repoDetailsHeaderData = header,
-                        repoTags = tags
+                val headerResult = headerDeferred.await()
+                val tagsResult = tagsDeferred.await()
+
+                if (headerResult.isSuccess) {
+                    val header = headerResult.getOrThrow()
+                    val tags = tagsResult.getOrElse { emptyList() }
+
+                    Result.success(
+                        RepoDetailsData(
+                            repoDetailsHeaderData = header,
+                            repoTags = tags
+                        )
                     )
-                )
-            } else {
-                Result.failure(headerResult.exceptionOrNull() ?: Exception("Unknown error"))
+                } else {
+                    Result.failure(
+                        headerResult.exceptionOrNull() ?: Exception("Unknown error")
+                    )
+                }
+            } catch (e: Throwable) {
+                Result.failure(e)
             }
-        } catch (e: Throwable) {
-            Result.failure(e)
         }
-    }
 }
